edb98f1583909e68644ffdd41918b4f4c47d94fd
sauravkumar2004 <121476013+sauravkumar2004@users.noreply.github.com>
2024-04-27 21:51:23 +0530

    Add files via upload

---
+++ BasicinputOutput.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class BasicinputOutput {
+    public static void main(String []ar){
+        Scanner a=new Scanner(System.in);
+        int n=a.nextInt();
+        int m=a.nextInt();
+        System.out.println(n+m);
+    }
+}
